30a2a7b1e5cf4578d73de813bfb3bec0607cbfa9
unknown <robertgicheha3@gmail.com>
2023-03-15 00:54:37 +0300

    worked on folder structure

---
+++ Backend/src/Config/index.ts
@@ -0,0 +1,46 @@
+import mssql from 'mssql'
+import dotenv from 'dotenv'
+import path from 'path'
+
+
+dotenv.config({ path:path.resolve(__dirname, '../../.env')  })
+
+
+const sqlConfig = {
+
+  user: process.env.DB_USER as string,
+  password: process.env.DB_PWD  as string,
+  database: process.env.DB_NAME as string,
+  server: 'localhost',
+  pool: {
+    max: 10,
+    min: 0,
+    idleTimeoutMillis: 30000
+  },
+  options: {
+    encrypt: true, // for azure
+    trustServerCertificate: true // change to true for local dev / self-signed certs
+  }
+}
+console.log('Running');
+
+const checkConnection =async () => {
+    try{
+        const x = await mssql.connect (sqlConfig)
+        if (x.connecting){
+            console.log("Connecting");
+            
+        }
+        if (x.connected){
+            console.log("Connected to the Database");
+            
+        }
+        
+    }
+    catch (error){
+        console.log(error);
+        
+    }
+    
+}
+checkConnection()

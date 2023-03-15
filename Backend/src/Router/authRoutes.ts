16d1d7729abcbc96142715ad16e140304d7b02a0
unknown <robertgicheha3@gmail.com>
2023-03-16 00:47:13 +0300

    worked on server.ts file

--- Backend/src/Router/authRoutes.ts
+++ Backend/src/Router/authRoutes.ts
@@ -1,16 +1,16 @@
 import express from "express";
 import { UserRegister , UserLogin,GetAllUsers,GetUserProfile,deleteUser,GetUserById} from "../Controllers/authUserController";
 
-const userRoutes = express.Router();
+const authrouter = express.Router();
 
 // ROUTES FOR USER
-userRoutes.route("/register").post(UserRegister);
-userRoutes.route("/login").post(UserLogin);
-userRoutes.route("/user/:id").get(GetUserById);
-userRoutes.route("/users").get(GetAllUsers);
-userRoutes.route("/profile/:user_id").get(GetUserProfile);
-userRoutes.route("/delete/:id").patch(deleteUser);
+authrouter.route("/register").post(UserRegister);
+authrouter.route("/login").post(UserLogin);
+authrouter.route("/user/:id").get(GetUserById);
+authrouter.route("/users").get(GetAllUsers);
+authrouter.route("/profile/:user_id").get(GetUserProfile);
+authrouter.route("/delete/:id").patch(deleteUser);
 
 
 
-export default userRoutes;
+export default authrouter;
---
+++ Backend/src/server.ts
@@ -0,0 +1,20 @@
+import express, { json } from 'express'
+// import router from './Router'
+import authrouter from './Router/authRoutes'
+import cors from 'cors'
+
+
+const app= express()
+
+//Register some Middlewares
+app.use(cors())
+app.use(json()) //adds a body to the Request
+
+//Register Routes
+app.use('/auth',authrouter)
+
+
+app.listen(4002,()=>{
+console.log("Running hee...");
+
+})
